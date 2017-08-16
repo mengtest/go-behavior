3bd7df01cdb419b4b11a5c52d1d0dfacc54fd26b
Back Yu <yhfszb@gmail.com>
2017-08-16 11:31:26 +0800

    node/common: 更新node的实现方法。

--- node/common/node.go
+++ node/common/node.go
@@ -7,15 +7,34 @@ import (
 )
 
 type Node struct {
+	ID       int
+	Parent   behavior.Noder
 	Children []behavior.Noder
+	//BeforeUnionType
+	BeforeAttachments []behavior.Attachment
 }
 
-func NewNoder(name string) behavior.Noder {
-	panic("Not imp")
+func NewNoder(id int) behavior.Noder {
+	return &Node{
+		ID:       id,
+		Children: make([]behavior.Noder, 0),
+	}
 }
 
 func (this *Node) Run(ctx context.Context) behavior.Status {
 	panic("Not imp")
+}
+
+func (this *Node) GetID() int {
+	return this.ID
+}
+
+func (this *Node) SetParent(n behavior.Noder) {
+	this.Parent = n
+}
+
+func (this *Node) GetParent() behavior.Noder {
+	return this.Parent
 }
 
 func (this *Node) GetChildren() []behavior.Noder {
