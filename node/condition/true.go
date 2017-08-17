3ef8e038bdf1e44aeaa2b5747a362aaf16cc061a
Back Yu <yhfszb@gmail.com>
2017-08-17 11:19:59 +0800

    增加condition/true节点

--- node/condition/true.go
+++ node/condition/true.go
@@ -1,1 +1,17 @@
 package condition
+
+import (
+	"context"
+
+	"github.com/szyhf/go-behavior"
+)
+
+//True节点总是返回Success
+
+type True struct {
+	Condition
+}
+
+func Run(ctx context.Context) behavior.Status {
+	return behavior.StatusSuccess
+}
